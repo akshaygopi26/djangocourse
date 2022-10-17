e13db3cc41875260ee3aa484fd7ec332bedb482e
Akshay G <22m0788@iitb.ac.in>
2022-10-17 11:25:21 +0530

    video 5 added items to sqlite

--- django_project/mobile/models.py
+++ django_project/mobile/models.py
@@ -4,11 +4,29 @@ from django.contrib.auth.models import User
 
 
 class Mobile(models.Model):                #each class is going to be its own table in the DB
+    Sno=models.CharField(max_length=100)
     brand=models.CharField(max_length=100)
-    base_color=models.TextField()                  #text field will take unrestricted text
-    model=models.TextField()
-    battery_capacity = models.IntegerField()
-    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    model=models.CharField(max_length=100)
+    base_color=models.CharField(max_length=100)
+    processor=models.CharField(max_length=100)
+    screen_size=models.CharField(max_length=100)
+    ROM=models.CharField(max_length=100)
+    RAM=models.CharField(max_length=100)
+    display_size=models.CharField(max_length=100)
+    num_rear_camera=models.CharField(max_length=100)
+    num_front_camera=models.CharField(max_length=100)
+    battery_capacity=models.CharField(max_length=100)
+    ratings=models.CharField(max_length=100)
+    num_of_ratings=models.CharField(max_length=100)
+    sales_price=models.CharField(max_length=100)
+    discount_percent=models.CharField(max_length=100)
+    sales=models.CharField(max_length=100)
+    units_sold=models.CharField(max_length=100)
+    brand=models.CharField(max_length=100)
+    # base_color=models.TextField()                  #text field will take unrestricted text
+    # model=models.TextField()
+    # battery_capacity = models.IntegerField()
+    # author=models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.brand
+        return f'{self.model}'
